a8057583961d04765711242575a2d74d468f1f2d
John Marzulli <v-jomarz@microsoft.com>
2021-01-25 23:43:45 -0800

    Demo how Top Level Calls work

--- Tools/DecisionTreeTool/DecisionTreeTool.cs
+++ Tools/DecisionTreeTool/DecisionTreeTool.cs
@@ -3,81 +3,69 @@ using System.Linq;
 using AiEngine.DecisionTree;
 using AiEngine.LearningBase;
 
-public class DecisionTreeTool
+if (args.Any())
 {
-    public static int Main(string[] args)
-    {
-        if (!args.Any())
-        {
-            Console.Error.WriteLine("Usage:  decision file.examples");
+    args.Aggregate(
+        true,
+        (current, exampleFile) => current & TestComplex(exampleFile));
+}
 
-            return -1;
+bool TestComplex(
+    string trainingDataFilename
+)
+{
+    var successCount = 0;
+    var failedCount = 0;
+
+    Console.WriteLine("Starting self test.");
+
+    var testTree = new DecisionTree();
+
+    bool outIsTestSuccessful = testTree.LoadTrainingData(trainingDataFilename);
+
+    if (outIsTestSuccessful)
+    {
+        Console.WriteLine($"Starting self test using example file `{trainingDataFilename}`");
+        Console.WriteLine("\nTraining");
+        testTree.Train();
+        Console.WriteLine("Resulting tree:");
+        Console.WriteLine(testTree);
+
+
+        foreach (Example example in testTree.Examples)
+        {
+            string correctAnswer = testTree.GetClass(example.ClassIdentifier);
+            string calculatedAnswer = testTree.Classify(example);
+
+            bool isExampleMathWithClassification = correctAnswer == calculatedAnswer;
+            successCount += isExampleMathWithClassification ? 1 : 0;
+            failedCount += isExampleMathWithClassification ? 0 : 1;
+
+            Console.WriteLine("----");
+            Console.WriteLine($"Example has outcome of '{correctAnswer}'");
+            Console.WriteLine($"Classified example '{example.ToString()}' as class `{calculatedAnswer}`");
+            Console.WriteLine($"SELF TEST {(isExampleMathWithClassification ? "SUCCEEDED" : "FAILED")}!");
         }
 
-        bool passed = args.Aggregate(
-            true,
-            (current, exampleFile) => current & TestComplex(exampleFile));
+        Console.WriteLine($"\nDone. {successCount} examples matched their classification, {failedCount} did not.");
+        Console.WriteLine($"Saving to {trainingDataFilename}.dts");
 
-        return passed ? 0 : -1;
+        string outputFileName = $"{trainingDataFilename}.dts";
+
+        testTree.SavePrebuiltTree($"{outputFileName}.reprocessed");
+
+        Console.WriteLine("Finished saving. Now attempting load!");
+
+        bool bIsLoadSuccess = testTree.LoadPrebuiltTree(outputFileName);
+
+        Console.WriteLine($"Save = {bIsLoadSuccess}");
+        Console.WriteLine("Loaded tree");
+        Console.WriteLine(testTree);
+    }
+    else
+    {
+        Console.Error.WriteLine($"ERROR - Unable to open test file '{trainingDataFilename}'");
     }
 
-    private static bool TestComplex(
-        string trainingDataFilename
-    )
-    {
-        var successCount = 0;
-        var failedCount = 0;
-
-        Console.WriteLine("Starting self test.");
-
-        var testTree = new DecisionTree();
-
-        bool outIsTestSuccessful = testTree.LoadTrainingData(trainingDataFilename);
-
-        if (outIsTestSuccessful)
-        {
-            Console.WriteLine($"Starting self test using example file `{trainingDataFilename}`");
-            Console.WriteLine("\nTraining");
-            testTree.Train();
-            Console.WriteLine("Resulting tree:");
-            Console.WriteLine(testTree);
-
-
-            foreach (Example example in testTree.Examples)
-            {
-                string correctAnswer = testTree.GetClass(example.ClassIdentifier);
-                string calculatedAnswer = testTree.Classify(example);
-
-                bool isExampleMathWithClassification = correctAnswer == calculatedAnswer;
-                successCount += isExampleMathWithClassification ? 1 : 0;
-                failedCount += isExampleMathWithClassification ? 0 : 1;
-
-                Console.WriteLine("----");
-                Console.WriteLine($"Example has outcome of '{correctAnswer}'");
-                Console.WriteLine($"Classified example '{example.ToString()}' as class `{calculatedAnswer}`");
-                Console.WriteLine($"SELF TEST {(isExampleMathWithClassification ? "SUCCEEDED" : "FAILED")}!");
-            }
-
-            Console.WriteLine($"\nDone. {successCount} examples matched their classification, {failedCount} did not.");
-            Console.WriteLine($"Saving to {trainingDataFilename}.dts");
-
-            string outputFileName = $"{trainingDataFilename}.dts";
-
-            testTree.SavePrebuiltTree($"{outputFileName}.reprocessed");
-
-            Console.WriteLine("Finished saving. Now attempting load!");
-
-            bool bIsLoadSuccess = testTree.LoadPrebuiltTree(outputFileName);
-
-            Console.WriteLine($"Save = {bIsLoadSuccess}");
-            Console.WriteLine("Loaded tree");
-            Console.WriteLine(testTree);
-        }
-        else
-        {
-            Console.Error.WriteLine($"ERROR - Unable to open test file '{trainingDataFilename}'");
-        }
-
-        return outIsTestSuccessful;
-    }
+    return outIsTestSuccessful;
 }
